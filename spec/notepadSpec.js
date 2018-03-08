950a325e7299baef00c189d23326456abc616223
Terry Allitt <tallitt88@gmail.com>
2018-03-08 16:15:56 +0000

    Create first test - passing when notepad is undefined - Dan is working on this

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,5 +1,11 @@
 describe('Notepad', function() {
   var notepad;
+  var note;
 
-  
+  it('shows a list of notes', function() {
+    notepad = new Notepad();
+    note = new Note(1, 'string');
+    notepad.content.push(note);
+    expect.toInclude(notepad.content, note);
+  });
 });
---
+++ src/notepad.js
@@ -0,0 +1,8 @@
+function Notepad() {
+  this.content = [];
+}
+
+function Note(id, string) {
+  this.id = id;
+  this.content = string;
+}
